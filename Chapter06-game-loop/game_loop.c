cbd3f03377ea96aed0f6100bcba16cfddb1979bf
Mike Lee <rockespecialee@gmail.com>
2020-12-28 18:35:55 -0800

    chapter 6 gameloop added

--- Chapter06-game-loop/game_loop.c
+++ Chapter06-game-loop/game_loop.c
@@ -1,3 +1,5 @@
+//emcc game_loop.c -o gameloop.html  --preload-file sprites -s NO_EXIT_RUNTIME=1 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS=['png'] -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap', 'ccall']" -s USE_SDL=2
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <emscripten.h>
