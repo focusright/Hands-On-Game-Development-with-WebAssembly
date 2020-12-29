a79bd4ba1637a8ef36c8b358bbbf09a26533485a
Mike Lee <rockespecialee@hotmail.com>
2020-12-29 03:00:49 -0800

    chapter 6 game objects compiled

--- Chapter06-game-object/main.cpp
+++ Chapter06-game-object/main.cpp
@@ -1,3 +1,5 @@
+//em++ main.cpp enemy_ship.cpp player_ship.cpp projectile.cpp projectile_pool.cpp -std=c++17 --preload-file sprites -s USE_WEBGL2=1 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS=['png'] -o game_objects.html
+
 #include "game.hpp"
 
 PlayerShip* player;
